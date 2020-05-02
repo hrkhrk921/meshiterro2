3c9645c455356dc6698efde45d077d1a8313cb5e
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-05-02 21:14:39 +0900

    コメント・いいね機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'df58b37bf192796669d0e779c925364a1653156d5a6771f5c84aa705fa752b93b0d7f05c174d6fd633dd1488422297ab8034b2d8a663faff1c06ba19de0e8b42'
